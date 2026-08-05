87fa4d81b0c63fa3c04d1aa0795372fa53a2c824
Chris.L <chris@fmafrica.com>
2026-08-05 13:50:57 +0200

    Versions / Tanks Color

--- screens/about.py
+++ screens/about.py
@@ -2,16 +2,24 @@ from textual.containers import VerticalScroll
 from textual.widgets import Static
 
 from widgets.header import LOGO
+from version import VERSION, BUILD, DATE_UPDATED, VENDOR
 
 
 class AboutPage(VerticalScroll):
+
     def compose(self):
-        yield Static("[bold]About[/bold]", classes="page-title")
-        yield Static(LOGO)
+
         yield Static(
-            "Version        0.2\n"
-            "Date Updated   04-07-2026\n"
-            "Build          dev\n"
-            "Vendor         Fuel Management Africa\n",
+            "[bold]About[/bold]",
+            classes="page-title"
+        )
+
+        yield Static(LOGO)
+
+        yield Static(
+            f"Version        {VERSION}\n"
+            f"Date Updated   {DATE_UPDATED}\n"
+            f"Build          {BUILD}\n"
+            f"Vendor         {VENDOR}\n",
             classes="info-block",
         )
